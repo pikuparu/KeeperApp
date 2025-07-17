4edf6e26709e921b53086815c278c7be02919804
PARVATHY <parvathysajeev19@gmail.com>
2025-07-17 15:15:46 +0200

    Updates

---
+++ precache-manifest.1c79497dbc16a8a21a29d1c94d536ba6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5d9f255de45f9c7c83d4f748e5f3bc",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "5cafb1087cad7b426a14",
+    "url": "/KeeperApp/static/js/2.bf1022b3.chunk.js"
+  },
+  {
+    "revision": "fb69164ac9492b9a56ec",
+    "url": "/KeeperApp/static/js/main.d6ab9df8.chunk.js"
+  },
+  {
+    "revision": "b0d95b6dd4928c8dea65",
+    "url": "/KeeperApp/static/js/runtime-main.b22b35da.js"
+  }
+]);
